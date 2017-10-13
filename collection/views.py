97fdd9eac33e909e2d80f264ec93f931439ba7d7
mustafasali <mustafasherazi@gmail.com>
2017-10-13 17:29:55 +0500

    Added navigation, new pages, and builtin templates

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # defining the variable
+    number = 6
+    thing = "Thing name"
+    # passing the variable to the view
+    return render(request, 'index.html', { 
+        'number': number, 
+        'thing': thing,
+        })
