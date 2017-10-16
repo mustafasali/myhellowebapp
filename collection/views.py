917f28904c7435a6bd6925948398cd3b7f9010e0
mustafasali <mustafasherazi@gmail.com>
2017-10-16 23:27:22 +0500

    Added things to database and display them via templates

--- collection/views.py
+++ collection/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
-    # defining the variable
-    number = 6
-    thing = "Thing name"
-    # passing the variable to the view
+    things = Thing.objects.all()
+    #things = Thing.objects.filter(name__contains='Hello')
     return render(request, 'index.html', { 
-        'number': number, 
-        'thing': thing,
+        'things': things,
         })
