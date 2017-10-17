1aea4905797d7d2109387b97ad077d7e8ec973bb
mustafasali <mustafasherazi@gmail.com>
2017-10-17 16:45:11 +0500

    Added forms to edit each entry.

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from collection.models import Thing
+from collection.forms import ThingForm
 
 # Create your views here.
 def index(request):
@@ -9,10 +10,32 @@ def index(request):
         'things': things,
         })
 
-def thing_detail(request, slug_template):
+def thing_detail(request, slug):
     # grab the object
-    thing = Thing.objects.get(slug=slug_template)
+    thing = Thing.objects.get(slug=slug)
     # and pass it on to the template
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
+        })
+
+def edit_thing(request, slug):
+    thing = Thing.objects.get(slug=slug)
+    # set the form we are using
+    form_class = ThingForm
+
+    # if we are coming to this view from a submitted form
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply to the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
         })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^$', 'collection.views.index', name='home'),
     url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    url(r'^things/(?P<slug_template>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
+    url(r'^things/(?P<slug>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
+    url(r'^things/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_thing', name='edit_thing'),
     url(r'^admin/', admin.site.urls),
 ]
